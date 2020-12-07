702c2a98be605046428498e3c5619799f4756598
Nathaniel Bedassie <nathanielbedassie@gmail.com>
2020-12-06 20:01:50 -0500

    Added Ids for Issues Table

--- php/issues.php
+++ php/issues.php
@@ -24,6 +24,8 @@ if($filter == "All"){
 
     $query = "SELECT issue.title, issue.id,issue.type,issue.status, issue.assigned_to, issue.created FROM Issues issue LEFT JOIN Users user ON issue.assigned_to = user.id  WHERE user.email  = '$email'"; 
 
+} else {
+  $query = "SELECT * FROM Issues issue INNER JOIN Users user ON issue.assigned_to = user.id";
 }
 
 try {
@@ -43,24 +45,25 @@ try {
 
 
 ?>
-<h2>Issues<button id="new-issue-btn">Create New Issue</button></h2>
-<h2>Filter By: <button id="byAll">ALL</button><button id="byOpen">OPEN</button><button id="byTickets">Tickets</button><br><br>
-<table class="table table-hover table-sm table-bordered">
-  <tr>
-    <th>Title</th>
-    <th>Type</th>
-    <th>Status</th>
-    <th>Assigned To</th>
-    <th>Created</th>
-  </tr>
-  <?php foreach ($results as $row): ?>
-  <tr>
-    <td id='<?= $row['id']?>'><?= $row['id'] ." ".$row['title'] ?></td>
-    <td><?= $row['type']?></td>
-    <td><?= $row['status']?></td>
-    <td><?=$fullname = $row['firstname'] . ' '. $row['lastname']?></td>
-    <td><?=$row['created']?></td>
-  </tr>
-  <?php endforeach; ?>
+<h2>Issues<button id="new-issue-btn">Create New Issue</button></h2><br>
+<h4>Filter By: <button id="byAll">ALL</button><button id="byOpen">OPEN</button><button id="byTickets">TICKETS</button></h4><br>
 
-</table>
+    <table>
+    <tr class="heading">
+        <td class="large-space headings"><b>Title</b></td>
+        <td class="same-space headings"><b>Type</b></td>
+        <td class="same-space headings"><b>Status</b></th>
+        <td class="same-space headings"><b>Assigned To</b></td>
+        <td class="same-space headings"><b>Created</b></td>
+    </tr>
+
+    <?php foreach ($results as $row): ?>
+    <tr id="sample">
+        <div><td id='<?= $row['id']?>' class="iss-title"><b>#<?= $row['id']?> </b><span><?= $row['title'] ?></span></td></div>
+        <div><td class="iss-type"><?= $row['type']?></td></div>
+        <div><td class="iss-stat"><div><?= $row['status']?></div></td></div>
+        <div><td class="iss-assigned"><?=$fullname = $row['firstname'] . ' '. $row['lastname']?></td></div>
+        <div><td class="iss-created"><?=$row['created']?></td></div>
+    </tr>
+    <?php endforeach; ?>
+    </table>
