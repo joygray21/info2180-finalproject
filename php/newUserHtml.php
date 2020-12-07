72f98964123298b1b30f8ee32888518ba32211db
Nathaniel Bedassie <nathanielbedassie@gmail.com>
2020-12-06 19:38:55 -0500

    Added template for newUserHtml.php

---
+++ php/newUserHtml.php
@@ -0,0 +1,8 @@
+<h2>New User</h2>
+        <div>
+            Firstname <br><input type="text" id="fname" name="fname"><br>
+            Lastname <br><input type="text" id="lname" name="lname"><br>
+            Password <br><input type="text" id="pword" name="pword"><br>
+            Email <br><input type="text" id="email" name="email"><br>
+            <input id="newuser-submit" type="submit">
+        </div>
